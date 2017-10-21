21e38943d684d2845d0dd8248b1a2decb96b69aa
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-10-21 20:24:06 +0330

    Commit 10/21/2017_8:23PM

---
+++ Assets/Scripts/DragControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragControl : MonoBehaviour {
+
+	private Vector3 ScreenPoint;
+	private Vector3 Offset;
+
+	void OnMouseDown (){
+
+		ScreenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		Offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z));
+
+	}
+
+	void OnMouseDrag (){
+	
+		Vector3 cursorPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint (cursorPoint) + Offset;
+		gameObject.transform.position = cursorPosition;
+	
+	}
+
+}
